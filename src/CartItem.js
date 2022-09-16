7be622a4104a2b7bc196dd8604926d62587dfa72
scotch77 <87890905+scotch77@users.noreply.github.com>
2022-09-16 15:08:56 +0530

    Plus button added

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,19 +1,42 @@
 import React from "react";
 
 class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
+    increaseQty = () => {
+        this.setState( {
+            qty : this.state.qty + 1  
+        })
+    }
+
     render() {
+        const {price, title, qty} = this.state;
+
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize: 25}}>Phone </div>
-                    <div style={{color: '#777'}}>Rs. 999 </div>
-                    <div style={{color: '#777'}}>Qty: 1 </div>
+                    <div style={{fontSize: 25}}>{title}</div>
+                    <div style={{color: '#777'}}>Rs. {price} </div>
+                    <div style={{color: '#777'}}>Qty: {qty} </div>
                     <div className="cart-item-actions">
                         {/*buttons*/}
-                        <img alt = "increase" className="action-icons" src="https://t3.ftcdn.net/jpg/03/30/25/74/240_F_330257485_WEd8LDmLkdp4vbJjhbLY263gV0OvsiIF.jpg"/>
+                        <img alt = "increase" 
+                            className="action-icons" 
+                            src="https://t3.ftcdn.net/jpg/03/30/25/74/240_F_330257485_WEd8LDmLkdp4vbJjhbLY263gV0OvsiIF.jpg"
+                            onClick={this.increaseQty}
+                        />
+                        
                         <img alt = "decrease" className="action-icons" src="https://t3.ftcdn.net/jpg/05/15/52/52/240_F_515525211_uXgI3FdevK7Gr2DioeChvCHsPuvNb580.jpg"/>
                         <img alt = "delete" className="action-icons" src="https://t4.ftcdn.net/jpg/01/90/89/15/240_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg"/>
                     </div>
